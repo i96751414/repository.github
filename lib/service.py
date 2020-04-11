499844d2c61551c2b6bd4a25cf2a7b3a8f4244bf
i96751414 <i96751414@gmail.com>
2020-04-11 18:33:02 +0100

    Update repository when port is modified

--- lib/service.py
+++ lib/service.py
@@ -1,10 +1,12 @@
 import logging
+import os
 import threading
+from xml.etree import ElementTree
 
 import xbmc
 
 from lib.httpserver import ThreadedHTTPServer, ServerHandler
-from lib.kodi import get_repository_port, set_logger
+from lib.kodi import ADDON_PATH, get_repository_port, set_logger
 from lib.routes import route_get_addons, route_get_addons_md5, route_get_assets
 
 # Register routes
@@ -13,8 +15,25 @@ ServerHandler.add_get_route("/addons.xml.md5", route_get_addons_md5)
 ServerHandler.add_get_route("/{w}/{p}", route_get_assets)
 
 
+def update_repository_port(port, xml_path=os.path.join(ADDON_PATH, "addon.xml")):
+    base_url = "http://127.0.0.1:{}/".format(port)
+    tree = ElementTree.parse(xml_path)
+    tree.find("extension[@point='xbmc.addon.repository']/info").text = base_url + "addons.xml"
+    tree.find("extension[@point='xbmc.addon.repository']/checksum").text = base_url + "addons.xml.md5"
+    tree.find("extension[@point='xbmc.addon.repository']/datadir").text = base_url
+    tree.write(xml_path, encoding="UTF-8", xml_declaration=True)
+
+
 class ServiceMonitor(xbmc.Monitor):
-    pass
+    def __init__(self):
+        super(ServiceMonitor, self).__init__()
+        self._port = get_repository_port()
+
+    def onSettingsChanged(self):
+        port = get_repository_port()
+        if port != self._port:
+            update_repository_port(port)
+            self._port = port
 
 
 class HTTPServerRunner(threading.Thread):
