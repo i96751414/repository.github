03bedad21f9ce6c9002ec983a33db022444ad955
i96751414 <i96751414@gmail.com>
2025-01-06 15:28:09 +0000

    Refactor cache

--- lib/cache.py
+++ lib/cache.py
@@ -1,20 +1,44 @@
 import time
 from functools import wraps
+from operator import attrgetter
 
 
-class _HashedSeq(list):
-    __slots__ = 'hash_value'
+class _CacheValue(object):
+    __slots__ = ["_value", "_modified"]
 
-    # noinspection PyMissingConstructor
-    def __init__(self, tup):
-        self[:] = tup
-        self.hash_value = hash(tup)
+    def __init__(self, value):
+        self._value = value
+        self._modified = time.time()
+
+    @property
+    def modified(self):
+        return self._modified
+
+    @property
+    def value(self):
+        return self._value
+
+    def expired(self, ttl):
+        return time.time() - self._modified > ttl
+
+    def update(self):
+        self._modified = time.time()
+
+
+class _HashedTuple(tuple):
+    __hash_value = None
 
     def __hash__(self):
-        return self.hash_value
+        hash_value = self.__hash_value
+        if hash_value is None:
+            self.__hash_value = hash_value = super(_HashedTuple, self).__hash__()
+        return hash_value
+
+    def __getstate__(self):
+        return {}
 
 
-def _make_key(args, kwds, typed, kwd_mark=(object(),), fast_types=(int, str)):
+def _make_key(args, kwargs, typed, kwd_mark=(object(),), fast_types=(int, str)):
     """
     Make a cache key from optionally typed positional and keyword arguments
 
@@ -26,17 +50,16 @@ def _make_key(args, kwds, typed, kwd_mark=(object(),), fast_types=(int, str)):
     saves space and improves lookup speed.
     """
     key = args
-    if kwds:
-        key += kwd_mark
-        for item in kwds.items():
-            key += item
+    sorted_kwargs = tuple(sorted(kwargs.items()))
+    if sorted_kwargs:
+        key += kwd_mark + sorted_kwargs
     if typed:
         key += tuple(type(v) for v in args)
-        if kwds:
-            key += tuple(type(v) for v in kwds.values())
+        if sorted_kwargs:
+            key += tuple(type(v) for _, v in sorted_kwargs)
     elif len(key) == 1 and type(key[0]) in fast_types:
         return key[0]
-    return _HashedSeq(key)
+    return _HashedTuple(key)
 
 
 def cached(seconds=60 * 60, max_size=128, typed=False, lru=False):
@@ -47,22 +70,24 @@ def cached(seconds=60 * 60, max_size=128, typed=False, lru=False):
             # no caching
             new_func = func
         else:
+            get_modified = attrgetter("modified")
+
             @wraps(func)
             def new_func(*args, **kwargs):
                 key = _make_key(args, kwargs, typed)
-                cache_entry = cache.get(key)
-                if cache_entry is None or time.time() - cache_entry[0] > seconds:
+                cache_entry = cache.get(key)  # type: _CacheValue
+                if cache_entry is None or cache_entry.expired(seconds):
                     # Check cache size first and clean if necessary
                     if len(cache) >= max_size:
-                        min_key = min(cache, key=lambda k: cache[k][0])
+                        min_key = min(cache, key=get_modified)
                         del cache[min_key]
 
                     result = func(*args, **kwargs)
-                    cache[key] = [time.time(), result]
+                    cache[key] = _CacheValue(result)
                 else:
                     if lru:
-                        cache_entry[0] = time.time()
-                    result = cache_entry[1]
+                        cache_entry.update()
+                    result = cache_entry.value
 
                 return result
 
