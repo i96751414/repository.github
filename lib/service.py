ab2a89e2f6fd1c5afd058905a8c9310c6ed0d7f4
i96751414 <i96751414@gmail.com>
2025-08-11 17:04:18 +0100

    #5 - Handle port mismatches

--- lib/service.py
+++ lib/service.py
@@ -3,13 +3,23 @@ import os
 import threading
 from xml.etree import ElementTree  # nosec
 
+try:
+    import urlparse
+except ImportError:
+    import urllib.parse as urlparse
+
 import xbmc
 
 from lib.entries import ENTRIES_PATH
 from lib.httpserver import threaded_http_server
-from lib.kodi import ADDON_PATH, get_repository_port, set_logger
+from lib.kodi import ADDON_PATH, get_repository_port, set_logger, notification, translate
 from lib.repository import Repository
 from lib.routes import add_repository_routes
+
+REPO_DIR_XPATH = "extension[@point='xbmc.addon.repository']/dir"
+REPO_INFO_XPATH = "info"
+REPO_CHECKSUM_XPATH = "checksum"
+REPO_DATADIR_XPATH = "datadir"
 
 set_logger()
 add_repository_routes(Repository(
@@ -19,10 +29,18 @@ add_repository_routes(Repository(
 def update_repository_port(port, xml_path=os.path.join(ADDON_PATH, "addon.xml")):
     base_url = "http://127.0.0.1:{}/".format(port)
     tree = ElementTree.parse(xml_path)
-    tree.find("extension[@point='xbmc.addon.repository']/dir/info").text = base_url + "addons.xml"
-    tree.find("extension[@point='xbmc.addon.repository']/dir/checksum").text = base_url + "addons.xml.md5"
-    tree.find("extension[@point='xbmc.addon.repository']/dir/datadir").text = base_url
+    dir_element = tree.find(REPO_DIR_XPATH)
+    dir_element.find(REPO_INFO_XPATH).text = base_url + "addons.xml"
+    dir_element.find(REPO_CHECKSUM_XPATH).text = base_url + "addons.xml.md5"
+    dir_element.find(REPO_DATADIR_XPATH).text = base_url
     tree.write(xml_path, encoding="UTF-8", xml_declaration=True)
+
+
+def validate_repository_port(port, xml_path=os.path.join(ADDON_PATH, "addon.xml")):
+    dir_element = ElementTree.parse(xml_path).find(REPO_DIR_XPATH)
+    ports = (urlparse.urlparse(dir_element.find(m).text).port for m in
+             (REPO_DATADIR_XPATH, REPO_INFO_XPATH, REPO_CHECKSUM_XPATH))
+    return all(port == p for p in ports)
 
 
 class ServiceMonitor(xbmc.Monitor):
@@ -33,6 +51,7 @@ class ServiceMonitor(xbmc.Monitor):
     def onSettingsChanged(self):
         port = get_repository_port()
         if port != self._port:
+            notification(translate(30021))
             update_repository_port(port)
             self._port = port
 
@@ -68,5 +87,8 @@ class HTTPServerRunner(threading.Thread):
 
 def run():
     port = get_repository_port()
+    if not validate_repository_port(port):
+        notification(translate(30020))
+        update_repository_port(port)
     with HTTPServerRunner(port):
         ServiceMonitor(port).waitForAbort()
